bf81f12b537de90c6ec9b42e26df6a962665d327
wendymasese <wendy.masese@student.moringaschool.com>
2024-04-04 00:31:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(names, query) {
+
+    const lowerCaseQuery = query.toLowerCase();
+    
+    return names.filter(name => name.toLowerCase() === lowerCaseQuery);
+}
+
+
+const names = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+const result = findMatching(names, "bobby");
+
+console.log(result); 
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+}
+
+function matchName(drivers, query) {
+
+    return drivers.filter(driver => driver.name === query);
+
+}
+
